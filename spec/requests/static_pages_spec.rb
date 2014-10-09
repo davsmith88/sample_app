040e0484e2a106b13ffb04bcd85f798615bf47b3
Dave Smith <davsmith88@gmail.com>
2014-10-09 11:00:35 +1000

    Refacter tests to reduce code smell

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+	let(:base_title) {"Ruby on Rails Tuturial Sample App"}
   describe "home page" do
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -10,7 +11,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
     	visit '/static_pages/home'
-    	expect(page).to have_title('Ruby on Rails Tuturial Sample App | Home')
+    	expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -22,7 +23,7 @@ describe "StaticPages" do
 
   	it 'should have the right title' do
   		visit '/static_pages/help'
-  		expect(page).to have_title('Ruby on Rails Tuturial Sample App | Help')
+  		expect(page).to have_title("#{base_title} | Help")
   	end
   end
 
@@ -34,7 +35,7 @@ describe "StaticPages" do
 
   	it 'should have the right title' do
   		visit '/static_pages/about'
-  		expect(page).to have_title('Ruby on Rails Tuturial Sample App | About')
+  		expect(page).to have_title("#{base_title} | About")
   	end
   end
 
@@ -45,7 +46,7 @@ describe "StaticPages" do
   	end
   	it "should have the title 'Contact'" do
   		visit '/static_pages/contact'
-  		expect(page).to have_title('Ruby on Rails Tuturial Sample App | Contact')
+  		expect(page).to have_title("#{base_title} | Contact")
   	end
   end
 
